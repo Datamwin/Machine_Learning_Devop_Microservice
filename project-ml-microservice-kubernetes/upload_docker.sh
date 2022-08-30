c8f1ced96335e6ff2118fd4c421c5d510652ce80
EC2 Default User <ec2-user@ip-172-31-70-77.ec2.internal>
2022-08-30 21:48:43 +0000

    Machine learning operation

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=datamwin/app 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag app $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
